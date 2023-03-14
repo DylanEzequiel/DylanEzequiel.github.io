39705cf2ba863d64d1a13dd1bd4d2254ad27ef60
Dylan Ezequiel <105092023+DylanEzequiel@users.noreply.github.com>
2023-03-14 13:40:12 -0500

    Actualización de colores

--- src/app/main.js
+++ src/app/main.js
@@ -38,3 +38,40 @@ button.onclick = () => {
 
   demo.innerHTML = calculate();
 }
+
+function selectColor() {
+  const COLORS = {
+    PRIMARY: [
+      "#F00", "#0F0", "#00F"
+    ],
+
+    SECONDARY: [
+      "#FF0", "#F0F", "#0FF"
+    ]
+  };
+
+  let input = prompt("Introduce un color");
+  
+  switch (input) {
+    case /rojo/i:
+      demo.innerHTML = COLORS.PRIMARY[0];
+      break;
+    case /verde/i:
+      demo.innerHTML = COLORS.PRIMARY[1];
+      break;
+    case /azul/i:
+      demo.innerHTML = COLORS.PRIMARY[2];
+      break;
+    case /amarillo/i:
+      demo.innerHTML = COLORS.SECONDARY[0];
+      break;
+    case /rosa/i:
+      demo.innerHTML = COLORS.SECONDARY[1];
+      break;
+    case /celeste/i:
+      demo.innerHTML = COLORS.SECONDARY[2];
+      break;
+    default:
+      demo.innerHTML = "¡Este color no está en la lista!";
+  }
+}
